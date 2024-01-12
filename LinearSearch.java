145d576f6320cdf004ed2536e98816b829b97572
Jessica Sampaio-Herlitz <102844641+jessampaio@users.noreply.github.com>
2024-01-12 12:12:56 -0500

    Linear Search

---
+++ LinearSearch.java
@@ -0,0 +1,72 @@
+public class LinearSearch {
+
+  public static void main(String[] args) {
+
+    System.out.println(min(-1,2,3));
+    System.out.println(min(-3,2,1));
+    System.out.println(min(3,3,1));
+    System.out.println(min(1,-3,1));
+    System.out.println(min(2,2,3)); // BUG
+
+    String s1 = "A";
+    String s2 = "F";
+
+    int cmp = s1.compareTo(s2);
+
+    System.out.println(cmp);
+
+    int[] array = {1,-2,-3,4,6,-2,-100,0};
+
+    
+    System.out.println("Num of negatives: " + numOfNegatives(array));
+    System.out.println("Min: " + findMin(array));
+
+  }
+   
+    public static int min(int a, int b, int c) {
+      if ((a < b) && (a < c)) {
+        return a;
+      }
+      if ((b < a) && (b < c)) {
+        return b;
+      }
+      return c;
+    }
+
+
+    /** 
+    *  initialize a counter called negativeCount to 0
+       for each element in the array {
+          if the current element is less than zero {
+            add one to negativeCount
+        }
+      }
+      return negativeCount 
+    *
+    */
+
+    public static int numOfNegatives(int[] arr) {
+      int negativeCount = 0;
+
+      for (int i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+          negativeCount++;
+        }
+      }
+
+      return negativeCount;
+    }
+
+
+    public static int findMin(int[] arr) {
+      int min = 0;
+
+      for (int i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+          min = arr[i];
+        }
+      }
+
+      return min;
+    }
+}
