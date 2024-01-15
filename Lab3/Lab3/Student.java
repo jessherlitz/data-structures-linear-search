e8f53665566f5e2f24c012970f0722ef216af6db
Jessica Sampaio-Herlitz <102844641+jessampaio@users.noreply.github.com>
2024-01-15 12:32:33 -0500

    total order lab - done

---
+++ Lab3/Lab3/Student.java
@@ -0,0 +1,77 @@
+package Lab3;
+/**
+ * Student.java
+ * A class to represent student data, for the
+ * purpose of illustrating order by Comparable
+ * and Comparator.
+ */
+public class Student implements Comparable<Student> {
+
+   private String fname;
+   private String lname;
+   private int section;
+
+    /** Creates a new student. */
+   public Student(String last, String first, int sec) {
+      lname = last;
+      fname = first;
+      section = sec;
+   }
+
+    /** Returns this student's first name. */
+   public String getFirstName() {
+      return fname;
+   }
+
+    /** Returns this student's last name. */
+   public String getLastName() {
+      return lname;
+   }
+
+    /** Returns this student's section. */
+   public int getSection() {
+      return section;
+   }
+
+    /**
+     * Implement compareTo so that students are ordered in the
+     * following way: in ascending order of last name, then in
+     * ascending order of first name, and then in ascending order
+     * of section.
+     */
+//    @Override
+//    public int compareTo(Student s) {
+//       int lastNameComparison = this.lname.compareTo(s.lname);
+//       if (lastNameComparison != 0) {
+//          return lastNameComparison;
+//       }
+
+//       int firstNameComparison = this.fname.compareTo(s.fname);
+//       if (firstNameComparison != 0) {
+//          return firstNameComparison;
+//       }
+
+//       return Integer.compare(this.section, s.section);
+//    }
+
+    @Override
+    public int compareTo(Student s) {
+
+        int cmp = this.lname.compareTo(s.lname);
+
+        if (cmp == 0) {
+            cmp = this.fname.compareTo(s.fname);
+            if (cmp == 0) {
+                return Integer.compare(this.section, s.section);
+            }
+        }
+
+        return cmp;
+    }
+
+    /** Returns a string representation of this student. */
+   @Override
+    public String toString() {
+      return section + ", " + lname + ", " + fname;
+   }
+}
